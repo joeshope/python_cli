8b422dbc466fc20cf06ed3f04d8f219c258d9003
joeshope <100427601+joeshope@users.noreply.github.com>
2024-03-26 16:23:04 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,74 @@
+import os
+import subprocess
+import argparse
+import time
+from datetime import datetime
+from git import Repo
+
+parser = argparse.ArgumentParser(description='file: input the file path to your repo list file')
+parser.add_argument("--file")
+args = parser.parse_args()
+repo_file = args.file
+
+if "GH_USERNAME" in os.environ:
+    username = os.environ['GH_USERNAME']
+else:
+    print("Enter your GH User")
+    username = input()
+if "GH_TOKEN" in os.environ:
+    gh_token = os.environ['GH_TOKEN']
+else:
+    print("Enter your GH Token")
+    gh_token = input()
+if "SNYK_TOKEN" in os.environ:
+    snyk_token = os.environ['SNYK_TOKEN']
+else:
+    print("Enter your Snyk API Token")
+    snyk_token = input()
+if not repo_file:
+    print("Enter the repo list file")
+    repo_file = input()
+if "SNYK_ORG" in os.environ:
+    snyk_org = os.environ['SNYK_ORG']
+else:
+    print("Enter the your Snyk org slug 'https://snyk.io/org/[orgslugname]'")
+    snyk_org = input()
+
+with open(repo_file, 'r') as file:
+    paths = file.read().splitlines()
+
+for r in paths:
+    if os.path.exists(os.path.join(os.getcwd(), f'{r}')):
+        print(f'{r} already cloned')
+    else:
+        Repo.clone_from(f"https://{username}:{gh_token}@github.com/{r}.git", f"{r}")
+
+for r in paths:
+    now = datetime.now()
+    timestamp = now.strftime("%Y%m%d.%H%M")
+    if os.name == 'nt':
+        os.chdir(f'./{r}')
+        os.system(f'snyk.exe auth {snyk_token}')
+        open_source = f'snyk.exe test --org={snyk_org} --all-projects --fail-fast --json-file-output=sca_results_{timestamp}.json'
+        os_result = subprocess.check_output([open_source], stderr=subprocess.STDOUT)
+        print(os_result)
+        static_code = f'snyk.exe code test --org={snyk_org} --json-file-output=sast_results_{timestamp}.json'
+        sc_result = subprocess.check_output([static_code], stderr=subprocess.STDOUT)
+        print(sc_result)
+        infra_code = f'snyk.exe iac test --org={snyk_org} --json-file-output=iac_results_{timestamp}.json'
+        iac_result = subprocess.check_output([infra_code], stderr=subprocess.STDOUT)
+        print(iac_result)
+        time.sleep(5)
+    else:
+        os.chdir(f'./{r}')
+        os.system(f'snyk auth {snyk_token}')
+        open_source = f'snyk test --org={snyk_org} --all-projects --fail-fast --json-file-output=sca_results_{timestamp}.json'
+        os_result = subprocess.check_output([open_source], stderr=subprocess.STDOUT)
+        print(os_result)
+        static_code = f'snyk code test --org={snyk_org} --json-file-output=sast_results_{timestamp}.json'
+        sc_result = subprocess.check_output([static_code], stderr=subprocess.STDOUT)
+        print(sc_result)
+        infra_code = f'snyk iac test --org={snyk_org} --json-file-output=iac_results_{timestamp}.json'
+        iac_result = subprocess.check_output([infra_code], stderr=subprocess.STDOUT)
+        print(iac_result)
+        time.sleep(5)
